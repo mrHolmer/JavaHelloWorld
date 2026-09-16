c324b9c63c2fb7d1b8f4fce2d42daeb2a7a7f4d7
Mr. Holmer <qholmer@schools.nyc.gov>
2026-09-16 14:38:44 -0400

    Added scanner.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class HelloWorld {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
         System.out.println("Hello World!");
         System.out.println("Hi");
         System.out.println("test");
+
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello " + name);
+
+        File f = new File("Class.txt");
+        
+        s.close();
+        s = new Scanner(f);
+        
+        String course = s.nextLine();
+
+        System.out.println("Welcome to " + course + "!");
+    
+
+
+        s.close();
+
+
     }
 }
